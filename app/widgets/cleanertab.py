353584e46b38255183c21932bbad02f5ff49371a
Guo Yunhe <i@guoyunhe.me>
2022-12-23 21:55:05 +0800

    remove fields

--- app/widgets/cleanertab.py
+++ app/widgets/cleanertab.py
@@ -4,7 +4,6 @@ from PySide6.QtWidgets import (
     QWidget,
     QVBoxLayout,
     QLabel,
-    QFrame,
 )
 
 from app.cleaners.journal import JournalCleaner
@@ -19,27 +18,8 @@ class CleanerTab(QWidget):
         journal_usage, journal_clean = JournalCleaner.usage()
         path_value_label = QLabel('Usage: ' + format_byte_size(journal_usage))
 
-        size_label = QLabel("Size:")
-        size = 1024
-        size_value_label = QLabel(f"{size} K")
-        size_value_label.setFrameStyle(QFrame.Panel | QFrame.Sunken)
-
-        last_read_label = QLabel("Last Read:")
-        last_read_value_label = QLabel('2022')
-        last_read_value_label.setFrameStyle(QFrame.Panel | QFrame.Sunken)
-
-        last_mod_label = QLabel("Last Modified:")
-        last_mod_value_label = QLabel('2022')
-        last_mod_value_label.setFrameStyle(QFrame.Panel | QFrame.Sunken)
-
         main_layout = QVBoxLayout()
         main_layout.addWidget(journal_label)
         main_layout.addWidget(path_value_label)
-        main_layout.addWidget(size_label)
-        main_layout.addWidget(size_value_label)
-        main_layout.addWidget(last_read_label)
-        main_layout.addWidget(last_read_value_label)
-        main_layout.addWidget(last_mod_label)
-        main_layout.addWidget(last_mod_value_label)
         main_layout.addStretch(1)
         self.setLayout(main_layout)
