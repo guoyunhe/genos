6799b0ccd422cb34c34532c965638b475677afb8
Guo Yunhe <i@guoyunhe.me>
2022-12-24 18:11:57 +0800

    optimize button

--- app/cleaner/cleanerpane.py
+++ app/cleaner/cleanerpane.py
@@ -17,28 +17,53 @@ class CleanerPane(QWidget):
     def __init__(self,  parent: QWidget):
         super().__init__(parent)
 
-        self.title_label = QLabel(self.title)
-        self.usage_label = QLabel('-')
-        self.clean_button = QPushButton(_('Clean'))
+        self.title_label = QLabel(self.title, self)
+
+        self.usage_label = QLabel('-', self)
+
+        self.clean_button = QPushButton(_('Clean'), self)
         self.clean_button.clicked.connect(self.clean)
+
+        self.optimize_button = QPushButton(_('Optimize'), self)
+        self.optimize_button.setEnabled(False)
+        self.optimize_button.setCheckable(True)
+        self.optimize_button.setChecked(False)
+        self.optimize_button.clicked.connect(self.optimize)
 
         main_layout = QHBoxLayout()
         main_layout.addWidget(self.title_label)
         main_layout.addStretch(1)
         main_layout.addWidget(self.usage_label)
         main_layout.addStretch(1)
+        main_layout.addWidget(self.optimize_button)
         main_layout.addWidget(self.clean_button)
         self.setLayout(main_layout)
 
         self.update()
 
+    @Slot()
+    def clean(self):
+        pass
+
+    @Slot()
+    def optimize(self):
+        pass
+
     def update(self):
         total_size = self.scan()
         self.usage_label.setText(format_byte_size(total_size))
 
+        if self.needOptimize():
+            self.optimize_button.setEnabled(True)
+            self.optimize_button.setChecked(False)
+            self.optimize_button.setText('Optimize')
+        else:
+            self.optimize_button.setEnabled(False)
+            self.optimize_button.setChecked(True)
+            self.optimize_button.setText('Optimized')
+
     def scan(self) -> int:
         return 0
 
-    @Slot()
-    async def clean(self):
-        pass
+    def needOptimize(self) -> bool:
+        return False
