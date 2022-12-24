5c41aae6c395aa02c4eeca4e3a5d97bfc0576d16
Guo Yunhe <i@guoyunhe.me>
2022-12-24 17:24:57 +0800

    comment

--- server/journal_clean.py
+++ server/journal_clean.py
@@ -2,5 +2,7 @@ from subprocess import PIPE, Popen
 
 
 def journal_clean():
+    # Mark the currently active journal logs as archive and create fresh new logs
     Popen('journalctl --rotate', shell=True).wait()
+    # Remove all archived journal logs
     Popen('journalctl --vacuum-files=1', shell=True).wait()
